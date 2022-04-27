db9b56560b1199b81789b3b8a1f6a7b4ffcfd4e9
FreshDrive <72299542+FreshDrive@users.noreply.github.com>
2022-04-27 17:51:08 +0200

    Add files via upload

---
+++ QuickMath.js
@@ -0,0 +1,10 @@
+
+var a = 2;
+var b = 2;
+var c = 1
+
+var result1 = a + b;
+var result2 = result1 - c;
+var resultText = a + " + "  + b + " is " + result1 + " - " + c + " is "  + result2 + ", QUICK MATH!";
+
+console.log(resultText);
